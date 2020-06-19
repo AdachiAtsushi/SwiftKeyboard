a37c42a11393c2246480337775eca7350fd2e666
Atsushi Adachi <atsuk0r05296@gmail.com>
2020-06-19 13:34:59 +0900

    ボタン押下時の挙動を追加(画面押下時、リターンキー押下時にキーボードを閉じる)

--- SwiftKeyboard/ViewController.swift
+++ SwiftKeyboard/ViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var logoImageView: UIImageView!
-    
-    
+        
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passWordTextField: UITextField!
     
@@ -24,15 +23,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userNameTextField.delegate = self
+        passWordTextField.delegate = self
     }
     
     // ボタン押下時のアクション定義
     @IBAction func login(_ sender: Any) {
+        // ログイン成功時の画像を切り替え
+        logoImageView.image = UIImage(named: "loginOK")
+        
         // 各テキストの値をラベルに渡す
         userNameLabel.text = userNameTextField.text
         passwordLabel.text = passWordTextField.text
         
         
+    }
+    
+    // タッチでキーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    // リターンキー押下時にキーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
 
